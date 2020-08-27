e61721e941f941be2a71848aa5ca40fc62326004
Santiago Bettinelli <santiago20121994@gmail.com>
2020-08-27 00:19:36 -0300

    primera parte de entrega 2 individual completada

--- js/products.js
+++ js/products.js
@@ -1,17 +1,43 @@
 var productsArray = [];
+var currentsortcriteria = undefined;
+var minprecio = undefined;
+var maxprecio = undefined;
+const ascendenteprecio = "AC-P";
+const descendenteprecio = "DC-P";
+const descendenterelevancia = "DC-R";
+
 
 var showSpinner = function(){
     document.getElementById("spinner-wrapper").style.display = "block";
-  }
+};
   
-  var hideSpinner = function(){
+var hideSpinner = function(){
     document.getElementById("spinner-wrapper").style.display = "none";
-  }
+};
+
+
+// function sortProducts( sortcriteria,array){
+//     let result = [];
+//     if ()
+// }
+
+
+
+
 
 function showProductsList(array){
     let htmlContentToAppend = "";
-    for(let i = 0; i < array.length; i++){
-        let product = array[i];
+    let arraycopia = array
+    if (minprecio!= undefined && maxprecio != undefined){
+        arraycopia = arraycopia.filter( producto =>
+            (producto.cost >= minprecio && producto.cost <= maxprecio));
+    }else if (minprecio != undefined || maxprecio != undefined){
+         arraycopia = arraycopia.filter( producto =>
+            ((minprecio != undefined && producto.cost >= minprecio) || (maxprecio != undefined && producto.cost <=maxprecio)))
+    };  
+
+    for(let i = 0; i < arraycopia.length; i++){
+        let product = arraycopia[i];
 
         htmlContentToAppend += `
         <div class="list-group-item list-group-item-action">
@@ -51,4 +77,29 @@ document.addEventListener("DOMContentLoaded", function (e) {
         }
         hideSpinner();
     })
+
+    document.getElementById("borrarRangoFiltrado").addEventListener("click", function (){
+        minprecio = undefined;
+        maxprecio = undefined;
+        document.getElementById("filtradoPrecioMinimo").value = "";
+        document.getElementById("filtradoPrecioMaximo").value = "";
+        showProductsList(productsArray);
+    })
+    document.getElementById("filtradoPrecio").addEventListener("click",function (){
+        minprecio = document.getElementById("filtradoPrecioMinimo").value;
+        maxprecio = document.getElementById("filtradoPrecioMaximo").value;
+        if ((minprecio != undefined && minprecio != "")&& parseInt(minprecio)>=0){
+            minprecio = parseInt(minprecio);
+        }else {
+            minprecio = undefined;
+        };
+        if ((maxprecio != undefined && maxprecio != "")&& parseInt(maxprecio)>=0){
+            maxprecio = parseInt(maxprecio);
+        }else {
+            maxprecio = undefined;
+        }
+
+        showProductsList(productsArray);
+    })
+
 });
